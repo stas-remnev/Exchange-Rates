f69e9e7cb4f88b240f82a37da57cbacc72417603
Stanislav Remnev <stas-remnev@yandex.ru>
2017-06-27 15:44:26 +0300

    +комментарии

--- Charts.js usage/server.js
+++ Charts.js usage/server.js
@@ -4,11 +4,6 @@ var fs = require('fs');
 xml2js = require('xml2js');
 var inspect = require('util-inspect');
 
-// Функция парсит локальный xml-файл
-function parseMyFile() {
-
-}
-
 // Загрузка и сохранение удаленного файла  
 var file = fs.createWriteStream("example.xml");
 var request = http.get("http://www.cbr.ru/scripts/XML_daily.asp?date_req=02/03/2002", function (response) {
@@ -22,9 +17,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 
   // Парсинг скачанного xml-файла
-  // parseMyFile();
   var parser = new xml2js.Parser();
-
   fs.readFile('example.xml', function (err, data) {
     parser.parseString(data, function (err, result) {
       console.log(inspect(result, false, null));
