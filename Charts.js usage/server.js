f5843a2d33bc3339e3e44ca3b0ba78029f49b8eb
Stanislav Remnev <stas-remnev@yandex.ru>
2017-06-27 14:21:29 +0300

    +получение удаленного xml-файла

--- Charts.js usage/server.js
+++ Charts.js usage/server.js
@@ -1,14 +1,23 @@
 var http = require('http');
 
+var fs = require('fs');
+
+
 
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
+
+
+  var file = fs.createWriteStream("example.xml");
+  var request = http.get("http://www.cbr.ru/scripts/XML_daily.asp?date_req=02/03/2002", function (response) {
+    response.pipe(file);
+  });
 });
 
 
 server.listen(6575, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
